9eca202a6fdb57873fa679c3f39f7e89aef60622
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-10-06 00:04:00 -0400

    Create Auth2.php

---
+++ src/epicmc/bridgeauth/task/Auth2.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace epicmc\bridgeauth\task;
+
+use pocketmine\scheduler\PluginTask;
+use epicmc\bridgeauth\BridgeAuth;
+use pocketmine\Server;
+
+
+class Auth2 extends PluginTask{
+
+    public function onRun($currentTick) {
+$sender->sendPopup(Authenticating... oOo");
+    }
+}
